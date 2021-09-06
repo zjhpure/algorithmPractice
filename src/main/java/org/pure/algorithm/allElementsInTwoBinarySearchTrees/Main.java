64d684e055daf19d2edcd71b7753d200a7e63382
pure <714331582@qq.com>
2021-09-06 20:55:52 +0800

    修正翻转二叉树，修正其中的数组层序遍历转为二叉树的方法，原来的方法如果数组中有null会错误，改为用队列辅助解决

---
+++ src/main/java/org/pure/algorithm/allElementsInTwoBinarySearchTrees/Main.java
@@ -0,0 +1,9 @@
+package org.pure.algorithm.allElementsInTwoBinarySearchTrees;
+
+/**
+ * @author zhangjianhua
+ * @date 2021-09-06 01:22
+ * @description
+ */
+public class Main {
+}
--- src/main/java/org/pure/algorithm/invertBinaryTree/Main.java
+++ src/main/java/org/pure/algorithm/invertBinaryTree/Main.java
@@ -16,7 +16,7 @@ public class Main {
         scanner.close();
 
         // 处理输入结果
-        int[] nums;
+        Integer[] nums;
         if ("[]".equals(str)) {
             // 当数组为空
             nums = null;
@@ -24,13 +24,17 @@ public class Main {
             // 当数组不为空
             String[] strs = str.split("\\[")[1].split("]")[0].split(",");
             int size = strs.length;
-            nums = new int[size];
+            nums = new Integer[size];
             for (int i = 0; i < size; ++i) {
-                nums[i] = Integer.parseInt(strs[i]);
+                if ("null".equals(strs[i])) {
+                    nums[i] = null;
+                } else {
+                    nums[i] = Integer.parseInt(strs[i]);
+                }
             }
         }
 
-        // 数组转为树
+        // 数组转为二叉树
         TreeNode root = arrayToTreeNode(nums);
 
         // 获取输出结果
@@ -38,61 +42,86 @@ public class Main {
         System.out.println(Arrays.toString(treeNodeToArray(result)));
     }
 
-    // 数组转为树，层序遍历，分层按照从上到下，从左到右的顺序
-    private static TreeNode arrayToTreeNode(int[] nums) {
+    // 数组转为二叉树，层序遍历，分层按照从上到下，从左到右的顺序
+    private static TreeNode arrayToTreeNode(Integer[] nums) {
+        // 利用队列辅助，特别需要注意支持数组元素为null，当数组元素为null时，该节点没有孩子，但是自身会占用一个数组元素
+        // 如果该节点的左孩子为null，右孩子不为null，那么数组的下一个元素会从右孩子的子节点开始，因为左孩子为null，没有子节点
+        // 例子：[0,null,2,null,4,null,6]，[1,null,2,3]，[0,1,null,null,4]，[4,2,7,1,3,6,9]
+
         if (nums == null || nums.length == 0) {
-            // 若树节点为空，直接返回空
+            // 若二叉树节点为空，直接返回空
             return null;
         }
 
-        // 创建树的根节点
-        TreeNode root = new TreeNode();
+        // 数组的开始下标
+        int i = 1;
 
-        // 树的根节点为数组第一个元素
-        root.val = nums[0];
+        // 先构造二叉树根节点
+        TreeNode root = new TreeNode(nums[0]);
 
-        // 递归构造树节点
-        createTreeNode(root, nums, 1);
+        // 定义当前的二叉树节点，保存临时值
+        TreeNode current;
 
-        // 返回树的根节点
+        // 定义当前数组的值
+        Integer value;
+
+        // 定义队列，层序遍历创建二叉树
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        // 先把二叉树根节点放进队列最后
+        queue.add(root);
+
+        // 遍历数组，构造二叉树
+        while (i < nums.length) {
+            // 队列出列第一个元素，获取当前二叉树节点
+            current = queue.poll();
+
+            // 获取数组的值，数组下标加1
+            value = nums[i++];
+
+            if (value != null) {
+                // 创建当前二叉树节点的左孩子
+                TreeNode left = new TreeNode(value);
+
+                if (current != null) {
+                    // 构造当前二叉树节点的左孩子
+                    current.left = left;
+                }
+
+                // 把当前二叉树节点的左孩子放进队列最后
+                queue.add(left);
+            }
+
+            if (i < nums.length) {
+                // 获取数组的值，数组下标加1
+                value = nums[i++];
+
+                if (value != null) {
+                    // 创建当前二叉树节点的右孩子
+                    TreeNode right = new TreeNode(value);
+
+                    if (current != null) {
+                        // 构造当前二叉树节点的右孩子
+                        current.right = right;
+                    }
+
+                    // 把当前二叉树节点的右孩子放进队列最后
+                    queue.add(right);
+                }
+            }
+        }
+
+        // 返回二叉树的根节点
         return root;
     }
 
-    // 递归构造树节点
-    private static void createTreeNode(TreeNode node, int[] nums, int k) {
-        if (k * 2 > nums.length) {
-            // 若节点的左节点大于了数组范围，递归回溯
-            return;
-        }
-
-        // 构造节点的左节点
-        node.left = new TreeNode();
-        // 若节点的值在数组的第k个，那么节点的左节点的值在数组的第k * 2个，下标需要减1，即为k * 2 - 1
-        node.left.val = nums[k * 2 - 1];
-
-        if (k * 2 + 1 > nums.length) {
-            // 若节点的右节点大于了数组范围，递归回溯
-            return;
-        }
-
-        // 构造节点的左节点
-        node.right = new TreeNode();
-        // 若节点的值在数组的第k个，那么节点的左节点的值在数组的第k * 2 - 1个，下标需要减1，即为k * 2
-        node.right.val = nums[k * 2];
-
-        // 构造节点的左节点的子节点
-        createTreeNode(node.left, nums, k * 2);
-        // 构造节点的右节点的子节点
-        createTreeNode(node.right, nums, k * 2 + 1);
-    }
-
-    // 树转为数组，层序遍历，分层按照从上到下，从左到右的顺序，这里因为是翻转二叉树，需要特殊处理，最后一层空的节点也要显示出来，显示为null，这样才能看出翻转后的二叉树的效果
+    // 二叉树转为数组，层序遍历，分层按照从上到下，从左到右的顺序，这里因为是翻转二叉树，需要特殊处理，最后一层空的节点也要显示出来，显示为null，这样才能看出翻转后的二叉树的效果
     private static Integer[] treeNodeToArray(TreeNode root) {
         // 定义两层列表，保存整体结果
         List<List<Integer>> list = new ArrayList<>();
 
         if (root == null) {
-            // 若树节点为空，直接返回空数组
+            // 若二叉树节点为空，直接返回空数组
             return new Integer[0];
         }
 
@@ -101,7 +130,7 @@ public class Main {
         // 定义队列
         Queue<TreeNode> queue = new LinkedList<>();
 
-        // 先把树的根节点放进队列最后
+        // 先把二叉树的根节点放进队列最后
         queue.add(root);
 
         // 循环，直到队列的长度为零，结束循环
@@ -122,7 +151,7 @@ public class Main {
                     temp.add(node.val);
 
                     if (node.left != null) {
-                        // 把当前树节点的左节点放进队列最后
+                        // 把当前二叉树节点的左孩子放进队列最后
                         queue.add(node.left);
                     } else {
                         // 子节点为空也要放进队列最后，为了适应看出翻转二叉树后的效果
@@ -130,7 +159,7 @@ public class Main {
                     }
 
                     if (node.right != null) {
-                        // 把当前树节点的右节点放进队列最后
+                        // 把当前二叉树节点的右孩子放进队列最后
                         queue.add(node.right);
                     } else {
                         // 子节点为空也要放进队列最后，为了适应看出翻转二叉树后的效果
@@ -185,6 +214,11 @@ public class Main {
             }
         }
 
+        if (nums[nums.length - 1] == null) {
+            // 若最后一个元素是null，删除最后一个元素，这种情况肯定是null落在右节点上，而且最后的位置，最后一个元素是null不要保留，如果null在左边才是要保留的
+            return Arrays.copyOf(nums, nums.length - 1);
+        }
+
         // 返回数组
         return nums;
     }
@@ -196,12 +230,12 @@ public class Main {
             return null;
         }
 
-        // 利用递归法，把树看成是根节点、左节点、右节点的整体，整体翻转根节点的左节点和右节点，如果左节点和右节点也是树，那么递归下去同样执行相同的方法，直到左节点和右节点为空时，递归开始回溯
+        // 利用递归法，把树看成是根节点、左孩子、右孩子的整体，整体翻转根节点的左孩子和右孩子，如果左孩子和右孩子也是树，那么递归下去同样执行相同的方法，直到左孩子和右孩子为空时，递归开始回溯
 
-        // 获取根节点的左节点
+        // 获取根节点的左孩子
         TreeNode left = invertTree(root.left);
 
-        // 根节点的左节点等于右节点，右节点等于左节点，实现翻转
+        // 根节点的左孩子等于右孩子，右孩子等于左孩子，实现翻转
         root.left = invertTree(root.right);
         root.right = left;
 
@@ -211,7 +245,7 @@ public class Main {
 
 }
 
-// 定义树
+// 定义二叉树
 class TreeNode {
 
     int val;
