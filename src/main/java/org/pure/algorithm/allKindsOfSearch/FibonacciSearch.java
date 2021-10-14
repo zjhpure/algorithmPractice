1693b3321adda09fb79af349d49bac25bb5e1da9
pure <714331582@qq.com>
2021-10-14 21:14:06 +0800

    各类查找弄懂斐波那契查找

--- src/main/java/org/pure/algorithm/allKindsOfSearch/FibonacciSearch.java
+++ src/main/java/org/pure/algorithm/allKindsOfSearch/FibonacciSearch.java
@@ -9,11 +9,10 @@ import java.util.Scanner;
  */
 public class FibonacciSearch {
 
-    // 原理尚未搞清，待解决
-
     // 斐波那契查找的序列必须是有序序列
     // 斐波那契查找是二分查找的一种提升算法，通过运用黄金比例的概念在数列中选择查找点进行查找，以提高查找效率
     // 随着斐波那契数列的递增，前后两个数的比值会越来越接近0.618，也就是黄金分割比例
+    // 如果数组长度达到没有斐波那契的要求，那么我们扩大数组来满足要求
 
     public static void main(String[] args) {
         // 获取输入结果
@@ -59,20 +58,22 @@ public class FibonacciSearch {
         // 定义中间指针
         int mid;
 
+        // 定义斐波那契数列的长度
         int k = 0;
 
+        // 构建斐波那契数列，在构建过程中，计算斐波那契数列的长度
         while (n > fib(k) - 1) {
             // 获取k值
             ++k;
         }
 
-        // 因为无法直接对原数组arr[]增加长度，所以定义一个新的数组
+        // 因为无法直接对原数组arr[]增加长度，所以定义一个新的数组，构建一个长度为斐波那契数列的长度的数组
         int[] temp = new int[fib(k)];
 
-        // 采用System.arraycopy()进行数组间的赋值
+        // 采用System.arraycopy()进行数组间的赋值，把原数组复制到新数组上
         System.arraycopy(arr, 0, temp, 0, arr.length);
 
-        // 对数组中新增的位置进行赋值
+        // 对数组中新增的位置进行赋值，如果数组长度达到没有斐波那契的要求，那么我们扩大数组来满足要求，但是后面新增的元素的值都等于原数组的最大值
         for (int i = n + 1; i <= fib(k) - 1; ++i) {
             temp[i] = temp[n];
         }
@@ -84,11 +85,11 @@ public class FibonacciSearch {
 
             if (target < temp[mid]) {
                 high = mid - 1;
-                // 对应上图中的左端，长度F[k-1]-1
+                // 对应上图中的左端，长度fib[k-1]-1
                 k = k - 1;
             } else if (target > temp[mid]) {
                 low = mid + 1;
-                // 对应上图中的右端，长度F[k-2]-1
+                // 对应上图中的右端，长度fib[k-2]-1
                 k = k - 2;
             } else {
                 // 当mid位于新增的数组中时，返回n
