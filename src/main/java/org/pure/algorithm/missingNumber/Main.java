6ac957161ade062613d596689efb7da3b13b141b
pure <714331582@qq.com>
2021-11-06 14:47:05 +0800

    修改丢失的数字，调整哈希集合法，添加方法2，数学法

--- src/main/java/org/pure/algorithm/missingNumber/Main.java
+++ src/main/java/org/pure/algorithm/missingNumber/Main.java
@@ -1,8 +1,6 @@
 package org.pure.algorithm.missingNumber;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * @author zhangjianhua
@@ -30,24 +28,49 @@ public class Main {
         System.out.println(result);
     }
 
+//    // 方法1，哈希集合法
+//    private static int missingNumber(int[] nums) {
+//        // 哈希集合法
+//
+//        // 定义哈希集合保存数组
+//        Set<Integer> set = new HashSet<>();
+//
+//        // 遍历数组nums，把nums的元素保存进哈希集合set中
+//        for (int num : nums) {
+//            set.add(num);
+//        }
+//
+//        // 遍历[0,n]范围的整数
+//        for (int i = 0; i <= nums.length; ++i) {
+//            if (!set.contains(i)) {
+//                // 若哈希集合set中不包含此元素，那么这个就是丢失的数字
+//                return i;
+//            }
+//        }
+//
+//        return -1;
+//    }
+
+    // 方法2，数学法
     private static int missingNumber(int[] nums) {
-        // 定义列表list保存数组
-        List<Integer> list = new ArrayList<>();
+        // 数学法
 
-        // 遍历数组nums，把nums的元素保存进列表list
+        // 获取数组的长度
+        int n = nums.length;
+
+        // 利用公式计算[0,n]范围整数的总和，total = (首项 + 末项) * 项数 / 2
+        int total = n * (n + 1) / 2;
+
+        // 计算数组的总和
+        int sum = 0;
+
+        // 遍历数组nums，通过累加计算数组的总和sum
         for (int num : nums) {
-            list.add(num);
+            sum += num;
         }
 
-        // 遍历[0,n]范围的整数
-        for (int i = 0; i <= nums.length; ++i) {
-            if (!list.contains(i)) {
-                // 若列表list中不包含此元素，那么这个就是丢失的数字
-                return i;
-            }
-        }
-
-        return 0;
+        // [0,n]范围整数的总和减去数组的总和，就是丢失的数字
+        return total - sum;
     }
 
 }
